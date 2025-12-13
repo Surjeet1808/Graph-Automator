2626a997bfd655fc474f65a20d7af05047ac8aee
Surjeet Dhakad <surjeetdhakad@128net6.unica.it>
2025-12-13 12:24:50 +0530

    ui fixed

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1196,7 +1196,6 @@ namespace GraphSimulator
                 UpdateScrollMeasureDisplay();
             }
         }
-        }
 
         /// <summary>
         /// Reset scroll measurement counter
